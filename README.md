973aa298ebcefd5be36056e7986a061e4717cf85
Joyce W <jianjoycewu@gmail.com>
2024-03-29 20:11:57 -0700

    init setup

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # stock_tracker
 Personal Portfolio Management
+
+
+## steps
+```
+script/setup
+script/server
+```
---
+++ home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Portfolio Management", layout="wide")
+
+st.write("# Streamlit Dashboard by J.W.")
